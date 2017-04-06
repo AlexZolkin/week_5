d95e7fed000cc8bf9a0d8c69eb2a9ee35f35aab2
alexzolkin19 <alexzolkin19@gmail.com>
2017-04-06 21:43:22 +0300

    added some get/set functions for working with nodes

--- src/task_2/Node.java
+++ src/task_2/Node.java
@@ -3,5 +3,39 @@ package task_2;
 /**
  * Created by Алексей on 06.04.2017.
  */
-public class Node {
+public class Node<K extends Comparable<K>, V>{
+    K key;
+    V value;
+    Node<K, V> left, right;
+
+    public Node(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+    public Node(){
+        this.key = null;
+        this.value = null;
+    }
+    public Node<K, V> getLeft(){
+        return this.left;
+    }
+    public Node<K, V> getRight(){
+        return this.right;
+    }
+    public K getKey(){
+        return this.key;
+    }
+    public V getValue(){
+        return this.value;
+    }
+    public V setValue(V value){
+        this.value = value;
+        return value;
+    }
+    public void setLeft(K key, V value){
+        this.left = new Node<K, V>(key, value);
+    }
+    public void setRight(K key, V value){
+        this.right = new Node<K, V>(key, value);
+    }
 }
